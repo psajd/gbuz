29c432974c520b8848999cebb894515791621a6f
Psajd <polpilik@gmail.com>
2024-12-18 15:27:35 +0300

    first commit

---
+++ src/main/java/com/psajd/gbuz/GbuzApplication.java
@@ -0,0 +1,13 @@
+package com.psajd.gbuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbuzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psajd/gbuz/GbuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psajd.gbuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
