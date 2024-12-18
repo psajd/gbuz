e9ee9443856aafad99e4faf9bfad0a38dc6dd73f
Psajd <polpilik@gmail.com>
2024-12-18 16:30:37 +0300

    create .env

--- src/main/java/com/psajd/gbuz/GbuzApplication.java
+++ src/main/java/com/psajd/gbuz/GbuzApplication.java
@@ -1,5 +1,6 @@
 package com.psajd.gbuz;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GbuzApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.out.println(dotenv.get("SPRING_DATASOURCE_URL"));
+		System.out.println(dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.out.println(dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+		// only for local tests
+		System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("server.port", dotenv.get("SERVER_PORT"));
+//		System.setProperty("logging.level.root", dotenv.get("LOGGING_LEVEL"));
+
 		SpringApplication.run(GbuzApplication.class, args);
 	}
 
